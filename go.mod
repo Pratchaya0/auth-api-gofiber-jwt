7f3bdfa05fd3af81a8f3a97a20bdc5c35e164e4d
Pratchaya0 <pratchayaworks@gmail.com>
2024-07-16 10:09:41 +0700

    add first unit test for check

--- go.mod
+++ go.mod
@@ -2,13 +2,18 @@ module github.com/Pratchaya0/auth-api-gofiber-jwt
 
 go 1.22.3
 
+require github.com/stretchr/testify v1.9.0
+
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2 // indirect
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
 	github.com/PuerkitoBio/purell v1.2.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/arsmn/fiber-swagger/v2 v2.31.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/spec v0.21.0 // indirect
@@ -33,9 +38,11 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/swaggo/files v1.0.1 // indirect
 	github.com/swaggo/files/v2 v2.0.1 // indirect
 	github.com/swaggo/swag v1.16.3 // indirect
 	github.com/urfave/cli/v2 v2.27.2 // indirect
---
+++ validators/user_test.go
@@ -0,0 +1,63 @@
+package validators
+
+import (
+	"testing"
+
+	"github.com/Pratchaya0/auth-api-gofiber-jwt/entities"
+	usercase "github.com/Pratchaya0/auth-api-gofiber-jwt/usecases"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockUserRepo struct {
+	getUserByIdFunc    func(id string) (entities.User, error)
+	getUserByEmailFunc func(email string) (entities.User, error)
+	listFunc           func() ([]entities.User, error)
+	saveFunc           func(user entities.User) (entities.User, error)
+}
+
+// GetUserByEmail implements repositories.UserRepository.
+func (m *mockUserRepo) GetUserByEmail(email string) (entities.User, error) {
+	panic("unimplemented")
+}
+
+// GetUserById implements repositories.UserRepository.
+func (m *mockUserRepo) GetUserById(issuer string) (entities.User, error) {
+	panic("unimplemented")
+}
+
+// List implements repositories.UserRepository.
+func (m *mockUserRepo) List() ([]entities.User, error) {
+	panic("unimplemented")
+}
+
+func (m *mockUserRepo) Save(user entities.User) (entities.User, error) {
+	return m.saveFunc(user)
+}
+
+func TestSaveUser(t *testing.T) {
+	t.Run("save user success", func(t *testing.T) {
+		repo := &mockUserRepo{
+			saveFunc: func(user entities.User) (entities.User, error) {
+				user = entities.User{
+					Name:     "name",
+					Email:    "email",
+					Password: []byte("$2a$14$wZ2Ix8QEY5QaUvvLSZ5rGe7oOW/BW6Bd5iI2ovEVzNqPqcE9n3QmG"),
+				}
+
+				return user, nil
+			},
+		}
+
+		service := usercase.NewUserUseCase(repo)
+
+		_, err := service.Save(
+			entities.User{
+				Name:     "name",
+				Email:    "email",
+				Password: []byte("$2a$14$wZ2Ix8QEY5QaUvvLSZ5rGe7oOW/BW6Bd5iI2ovEVzNqPqcE9n3QmG"),
+			},
+		)
+		assert.NoError(t, err)
+	})
+
+}
